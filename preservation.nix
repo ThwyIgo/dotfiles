284a1fedb216eac0d69eae683559b3fcad1d709a
ThwyIgo <thiagopachecorocha@hotmail.com>
2026-05-19 23:11:19 -0300

    hashedPasswordFile inside /persistent

--- preservation.nix
+++ preservation.nix
@@ -4,11 +4,13 @@
 
     preserveAt."/persistent" = {
       directories = [
+        "/etc/nixos"
         {
-          directory = "/etc/nixos";
-          inInitrd = true;
-        }
-        {
+          # NixOS dynamically generates the sshd_config file and places
+          # a symlink in /etc/ssh during the activation phase. Later in the boot process, the persistent /etc/ssh directory is mounted over the
+          # actual /etc/ssh directory. This hides the dynamically generated sshd_config symlink, which causes the SSH daemon to fail with the
+          # error: /etc/ssh/sshd_config: No such file or directory.
+          # inInitrd = true; fixes the problem above
           directory = "/etc/ssh";
           inInitrd = true;
         }
